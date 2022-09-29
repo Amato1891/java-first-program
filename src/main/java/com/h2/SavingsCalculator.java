4e1b6c0125f11f83fddd4312bb37de3180047826
Jim Amato <Amato1891@yahoo.com>
2022-09-29 14:37:03 -0400

    Completed module

---
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -0,0 +1,51 @@
+package com.h2;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+
+public class SavingsCalculator {
+    private float[] credits;
+    private float[] debits;
+
+    public SavingsCalculator(float[] credits, float[] debits) {
+        this.credits = credits;
+        this.debits = debits;
+    }
+
+    private float sumOfCredits() {
+        float sum = 0.0f;
+
+        for (int i = 0; i < credits.length; i++) {
+            sum += credits[i];
+        }
+        return(sum);
+    }
+
+    private float sumOfDebits() {
+        float sum = 0.0f;
+
+        for (int i = 0; i < debits.length; i++) {
+            sum += debits[i];
+        }
+        return(sum);
+    }
+
+    private static int remainingDaysInMonth(LocalDate date) {
+
+        YearMonth yearMonth = YearMonth.of(date.getYear(), date.getMonth());
+        int totalDaysInMonth = yearMonth.lengthOfMonth();
+        int remainingDays = date.getDayOfMonth() - totalDaysInMonth;
+        return(remainingDays);
+    }
+
+    public float calculate() {
+        return(sumOfDebits() - sumOfCredits());
+    }
+
+    public static void main(String[] args) {
+        String[] creditsAsString = args[0].split(",");
+        String[] debitsAsString = args[1].split(",");
+//        Float credits = new Float;
+    }
+
+}
